bceb26a05601793a1dd84c7c1a5ec3c0195ccd23
ItsLeMax <lemaxyt1243@gmail.com>
2024-05-18 06:37:38 +0200

    Added reason for one error type and grouped two integer values

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const validLetters = "abcdefghijklmnopqrstuvwxyz1234567890+#,.-<>;:_'*^";
 const intendedLength = process.argv[2] || 32;
+
+const length = {
+    maximum: 150,
+    minimum: 9
+}
+
 const colors = {
     error: "\x1b[31m",
     reset: "\x1b[0m"
 };
+
+const reasoning = {
+    de_DE: "würden den Sinn des Skripts zerstören: Ein starkes Passwort zu generieren.",
+    en_US: "would defeat the purpose of this script: to generate a powerful password."
+}
 
 if (isNaN(intendedLength)) {
     console.error(colors.error + "Only Numbers are allowed!", "Nur Nummern sind erlaubt!" + colors.reset);
     return;
 }
 
-const maximum = 150;
-if (intendedLength > maximum) {
-    console.error(colors.error + `Better choose ${maximum} signs or less!`, `Wähle besser ${maximum} oder weniger Zeichen!` + colors.reset);
+if (intendedLength > length.maximum) {
+    console.error(
+        colors.error + `Better choose ${length.maximum} signs or less! More ${reasoning.en_US}`,
+        `Wähle besser ${length.maximum} oder weniger Zeichen! Weitere ${reasoning.de_DE}` + colors.reset
+    );
     return;
 }
 
-const minimum = 9;
-if (intendedLength < minimum) {
-    console.error(colors.error + `Better choose ${minimum} signs or more!`, `Wähle besser ${minimum} oder mehr Zeichen!` + colors.reset);
+if (intendedLength < length.minimum) {
+    console.error(
+        colors.error + `Better choose ${length.minimum} signs or more! Less ${reasoning.en_US}`,
+        `Wähle besser ${length.minimum} oder mehr Zeichen! Weniger ${reasoning.de_DE}` + colors.reset
+    );
     return;
 }
 
